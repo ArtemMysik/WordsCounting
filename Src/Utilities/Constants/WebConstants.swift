c315c304a875c79ee6ade550553e54178cfbe647
  Artem <artemmysik@gmail.com>
2018-05-21 21:46:46 +0300

    updated project hierarchy

---
+++ Src/Utilities/Constants/WebConstants.swift
@@ -0,0 +1,17 @@
+//
+//  WebConstants.swift
+//  WordsCount
+//
+//  Created by   Artem on 21.05.18.
+//  Copyright © 2018   Artem. All rights reserved.
+//
+
+import Foundation
+
+enum WebConstants {
+  
+  struct Router {
+    static let baseURL = "https://apiecho.cf/doc/swagger.json"
+  }
+  
+}
