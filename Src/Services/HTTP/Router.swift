d9362037d10b37d8de7287602c8903cc5edaf710
  Artem <artemmysik@gmail.com>
2018-05-25 18:56:58 +0300

    Updated router

--- Src/Services/HTTP/Router.swift
+++ Src/Services/HTTP/Router.swift
@@ -97,7 +97,7 @@ extension Router: URLRequestConvertible {
         var urlRequest = URLRequest(url: fullURL)
         urlRequest.addValue(WebConstants.Router.contentTypeJSONValue,
                             forHTTPHeaderField: WebConstants.Router.contentTypeHeader)
-        let user = APIClient.shared.user
+        
         if let accessToken = APIClient.shared.token {
             urlRequest.addValue(WebConstants.Router.tokenPrefixValue + accessToken,
                                 forHTTPHeaderField: WebConstants.Router.authorizationHeader)
